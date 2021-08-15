e4fbd41a17721dde1f5960d82498dcc41854ec4d
Rathod Ritesh <Rathodriteshrock@gmail.com>
2021-08-16 00:55:04 +0530

    Init Commit and Create Component of The Header ,Main And Footer

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  Container, Nav } from 'react-bootstrap';
+function header() {
+    return (
+        <header>
+            <Container>
+                <Nav>
+
+                </Nav>
+            </Container>
+        </header>
+    )
+}
+
+export default header
