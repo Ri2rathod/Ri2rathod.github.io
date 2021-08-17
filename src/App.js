14a9f90d9a108e05447fdd33c61f7bbe73114fe9
Rathod Ritesh <Rathodriteshrock@gmail.com>
2021-08-18 00:21:36 +0530

    init commit and implementing header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/customize-Bootstrap.scss';
+import './assets/css/typography.css';
+
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,24 @@
+// import {  } from 'bootstrap';
 import React from 'react'
-import {  Container, Nav } from 'react-bootstrap';
+import logo from '../assets/images/logo.png';
+import {  Container, Nav ,Navbar ,Button} from 'react-bootstrap';
 function header() {
     return (
         <header>
-            <Container>
-                <Nav>
-
-                </Nav>
-            </Container>
+               <Navbar bg="light" expand="lg">
+                    <Container >
+                        <Navbar.Brand href="#home"> <img src={logo} alt="logo" /> </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav " className=" flex-grow-0">
+                        <Nav className="mr-auto ">
+                            <Nav.Link href="#home" className="mx-md-4 mx-2 text-primary">Home</Nav.Link>
+                            <Nav.Link href="#link" className="mx-md-4 mx-2 text-primary">Link</Nav.Link>
+                            <Nav.Link href="#link" className="mx-md-4 mx-2 text-primary">About Us</Nav.Link>
+                            <Button className="mx-md-4 mx-2"> Work</Button>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                    </Navbar>
         </header>
     )
 }
