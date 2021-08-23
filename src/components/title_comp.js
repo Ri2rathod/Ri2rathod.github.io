0d24d9929949980aec975aaedc23474fc6ea66e7
Rathod Ritesh <Rathodriteshrock@gmail.com>
2021-08-23 23:39:00 +0530

    creating title component

--- src/components/title_comp.js
+++ src/components/title_comp.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import svg from './../assets/images/svg/pattern.svg';
 
-export default function title_comp({bool}) {
+export default function title_comp({bool,heading}) {
+
+    let text=String(heading);
+    let style={
+        "margin-bottom":"65px"
+    }
 
     return (
         
-        <div className={`d-flex justify-content-between align-items-center ${bool=="true"?"flex-row-reverse":''} `}>
-            <h2 className="title word-nobreak">About My Selfs</h2>
-            <hr className="flex-fill" />
+        <div className={`d-flex justify-content-between align-items-center ${bool==="true"?"flex-row-reverse":''} `} style={style}>
+            <h2 className="title word-nobreak display-5"> 
+                <span className="heading_before_style position-relative">{text.slice(0,2)}</span>
+                <span>{text.slice(2)}</span> 
+            </h2>
+            <hr className="flex-fill " />
             <img className="pattern" src={svg} alt="sample"/>
         </div>
     )
