224e7fb15fc9152e4926b5dd0b16d3dbed4f8492
Rathod Ritesh <Rathodriteshrock@gmail.com>
2021-08-23 23:41:45 +0530

    create desc component

---
+++ src/components/desc.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function desc() {
+  
+    return (
+        <p className="text-muted col-12 col-md-8 text-capitalize desc">ince beginning my journey as a freelance designer nearly 10 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p>
+    )
+}
